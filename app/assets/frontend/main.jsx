01ab0c49d6570d47ef0e1e6025a7f4a79b656f30
Chase Sinclair <chase.sinclair@cerner.com>
2017-03-28 13:53:31 -0500

    Configured webpack and created basic Tweet React component

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,7 +1,11 @@
+import TweetBox from './components/TweetBox';
+
 class Main extends React.Component {
   render() {
     return (
-      <h1>Hello from the component</h1>
+      <div className="container">
+        <TweetBox />
+      </div>
     );
   }
 }
