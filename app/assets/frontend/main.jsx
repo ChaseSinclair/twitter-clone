139c6ac41458a5d83540b9db607c3876c3302a32
Chase Sinclair <chase.sinclair@cerner.com>
2017-04-05 11:34:26 -0500

    Set up Tweet list

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,10 +1,18 @@
-import TweetBox from './components/TweetBox';
+import TweetBox from "./components/TweetBox"
+import TweetsList from "./components/TweetsList"
+
+let mockTweets = [
+  { id: 1, name: 'Chase Sinclair', body: 'My #FirstTweet' },
+  { id: 2, name: 'Chase Sinclair', body: 'My #SecondTweet' },
+  { id: 3, name: 'Chase Sinclair', body: 'My #ThirdTweet' }
+];
 
 class Main extends React.Component {
   render() {
     return (
       <div className="container">
         <TweetBox />
+        <TweetsList tweets={mockTweets} />
       </div>
     );
   }
